d180cfd33dc843ca6151a92528704bcb2bc7581b
ronah289 <ronald.kibet@student.moringaschool.com>
2021-12-08 08:50:59 -0900

    define constants - api base url and api key

---
+++ app/src/main/java/com/example/giantbombapi/Constants.java
@@ -0,0 +1,8 @@
+package com.example.giantbombapi;
+
+public class Constants {
+
+    public static final String GIANT_BOMB_BASE_URL = "https://www.giantbomb.com/api/";
+    public static final String GIANT_BOMB_API_KEY = BuildConfig.GIANT_BOMB_API_KEY;
+
+}
