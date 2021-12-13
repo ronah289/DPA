9f2b63952917e50d16bc0829faa6a0caf34c6731
ronah289 <ronald.kibet@student.moringaschool.com>
2021-12-12 15:47:34 -0900

    store typed user name in shared preferences

--- app/src/main/java/com/example/giantbombapi/ui/MainActivity.java
+++ app/src/main/java/com/example/giantbombapi/ui/MainActivity.java
@@ -8,18 +8,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.giantbombapi.Constants;
 import com.example.giantbombapi.R;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.editTextPersonName) EditText my_name;
     @SuppressLint("NonConstantResourceId")
@@ -30,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
 
         go_button.setOnClickListener(this);
     }
@@ -44,14 +54,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 popName.show();
             }
             else{
+                addToSharedPrefs(username);
                 String nonBlankUsername = username.toUpperCase(Locale.ROOT);
                 Toast popName = Toast.makeText(MainActivity.this,nonBlankUsername,Toast.LENGTH_LONG);
                 popName.setGravity(Gravity.CENTER, 0, -200);
                 popName.show();
                 Intent intent = new Intent(MainActivity.this, MoviesListActivity.class);
-                intent.putExtra("username",nonBlankUsername);
                 startActivity(intent);
             }
         }
             }
+    private void addToSharedPrefs(String username) {
+        mEditor.putString(Constants.USER_NAME, username).apply();
+    }
     }
