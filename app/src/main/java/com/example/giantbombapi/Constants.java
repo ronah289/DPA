4e69424aabd197eb936746443f1015daddc708e3
ronah289 <ronald.kibet@student.moringaschool.com>
2021-12-12 15:46:19 -0900

    define user_name constant for shared preferences

--- app/src/main/java/com/example/giantbombapi/Constants.java
+++ app/src/main/java/com/example/giantbombapi/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
     public static final String GIANT_BOMB_BASE_URL = "https://www.giantbomb.com/api/";
     public static final String GIANT_BOMB_API_KEY = BuildConfig.GIANT_BOMB_API_KEY;
+    public static final String USER_NAME = "username";
 
 }
