087fe0a02767fb4de06bf85029a6b27b48270956
ronah289 <ronald.kibet@student.moringaschool.com>
2021-12-14 07:37:50 -0900

    add create account activity

---
+++ app/src/main/java/com/example/giantbombapi/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.giantbombapi.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.giantbombapi.R;
+import android.os.Bundle;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
