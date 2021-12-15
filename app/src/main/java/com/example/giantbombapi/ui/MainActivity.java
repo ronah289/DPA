47954aea78803304d36551209c1f57fade94bde4
ronah289 <ronald.kibet@student.moringaschool.com>
2021-12-14 21:51:45 -0900

    add code to redirect to login activity on logout

--- app/src/main/java/com/example/giantbombapi/ui/MainActivity.java
+++ app/src/main/java/com/example/giantbombapi/ui/MainActivity.java
@@ -67,6 +67,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void exit_app() {
         FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
