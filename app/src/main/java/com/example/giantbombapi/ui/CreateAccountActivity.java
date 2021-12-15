f5f4bd9445819fc65fa1e9b59d31bd93ed035cd4
ronah289 <ronald.kibet@student.moringaschool.com>
2021-12-14 21:05:41 -0900

    implement app redirect from create account activity to main activity

--- app/src/main/java/com/example/giantbombapi/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/giantbombapi/ui/CreateAccountActivity.java
@@ -1,9 +1,11 @@
 package com.example.giantbombapi.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.giantbombapi.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -21,6 +23,7 @@ import butterknife.ButterKnife;
 public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.editTextPersonName) EditText personName;
@@ -47,6 +50,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         goToLogin.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
     }
 
     @Override
@@ -79,5 +83,34 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                                 Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
